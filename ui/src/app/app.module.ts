b60d2101c313c2ad0acf0b4b440dbb1a728b0a2b
Spencer Young <spjy@hawaii.edu>
2022-10-28 22:43:00 -0700

    Remove CKEditor import in module

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -22,7 +22,6 @@ import { BillFetchActionComponent } from './bill-list-components/bill-fetch-acti
 import { ToastComponent } from './toast/toast.component';
 import { BillUpdateIntervalComponent } from './bill-list-components/bill-update-interval/bill-update-interval.component';
 import { TestimonyComponent } from './testimony/testimony.component';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
